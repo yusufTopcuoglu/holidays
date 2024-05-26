b9f7db73f5f3fa6717276d1f41f2fdc025a62ac0
yusuftopcuoglu <yusuf.topcuoglu531@gmail.com>
2024-05-26 21:00:55 +0300

    firs commit

---
+++ script.js
@@ -0,0 +1,53 @@
+let holidays
+let selectedCountry = 'turkey';
+let selectedYear = 2024
+
+
+fetch('./holidays.json')
+    .then((response) => response.json())
+    .then(data => {
+        holidays = data
+        setMainContent();
+    }
+)
+
+
+function showYear(year) {
+    selectedYear = year
+
+    setMainContent()
+}
+
+function updateCountry() {
+    const countrySelect = document.getElementById('country-select');
+    selectedCountry = countrySelect.value;
+
+    setMainContent()
+}
+
+function setMainContent() {
+    const mainContent = document.getElementById('main-content');
+    const yearHolidays = holidays[selectedCountry][selectedYear];
+
+    console.log(yearHolidays[0])
+    if (yearHolidays) {
+        mainContent.innerHTML = `
+            <h2>Holidays in ${selectedYear} (${selectedCountry.toUpperCase()})</h2>
+            <ul class="styled-list">
+                ${yearHolidays.map(holiday =>
+            `<li class="list-item">
+                        <div class="item-header">${holiday.name}</div>
+                        <div class="item-description">
+                            <ul>
+                                ${holiday.date.map(dateVal => `<li>${dateVal}</li>`).join('')}
+                            </ul>
+                        </div>
+                    </li>
+                    `)
+            .join('')}
+            </ul>
+        `;
+    } else {
+        mainContent.innerHTML = `<h2>No holidays found for ${selectedYear} (${selectedCountry.toUpperCase()})</h2>`;
+    }
+}
